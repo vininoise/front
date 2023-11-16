9d4646dab2e94371501f62edc36fc0e8f46f6449
Vinicius franco <vinicius.franco@alu.unibalsas.edu.br>
2023-11-16 20:48:24 -0300

    new version

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_restful import Resource, Api
 from joblib import load
 import pandas as pd
 
-app = Flask(__name__, template_folder='template')  
+app = Flask(__name__, template_folder='template')  # Defina o diretório dos templates
 api = Api(app)
 modelo = load('ML.joblib')
 
 class Pricing(Resource):
-    def get(self):
+    @app.route("/")
+    def hello_world():
         return render_template("form.html")
 
-    def post(self):
+    @app.route('/form', methods=['POST'])
+    def form():
+
         product_name_length = request.form.get('product_name_length')
         product_description_length = request.form.get('product_description_length')
         product_photos_qty = request.form.get('product_photos_qty')
@@ -20,6 +23,7 @@ class Pricing(Resource):
         product_height_cm = request.form.get('product_height_cm')
         product_width_cm = request.form.get('product_width_cm')
         product_category_name = request.form.get('product_category_name')
+
 
         input_df = pd.DataFrame({
             'product_name_length': [product_name_length],
@@ -32,10 +36,13 @@ class Pricing(Resource):
             'product_category_name': [product_category_name]
         })
 
+        # Faça a previsão usando o modelo
         predict = modelo.predict(input_df)[0]
+
+        # Renderize o template com o resultado da previsão
         return render_template('result.html', prediction=predict)
 
-api.add_resource(Pricing, '/form')
+api.add_resource(Pricing, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- template/script.js
+++ template/script.js
@@ -1,31 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var form = document.getElementById('pricing-form');
+    var form = document.getElementById('pricingForm');
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
         // Coletar dados do formulário
         var formData = new FormData(form);
         var data = {};
-
         formData.forEach(function (value, key) {
             data[key] = value;
         });
-        
-        // Enviar dados para o servidor via AJAX
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: JSON.stringify(data),
-            redirect: 'follow'
-        };
 
-        fetch("https://web-production-30b1.up.railway.app/", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+        // Enviar dados para o servidor via AJAX
+        fetch('/form', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(result => {
+            // Manipular o resultado retornado do servidor
+            console.log(result);
+            // Aqui você pode atualizar a interface do usuário com o resultado, se necessário
+        })
+        .catch(error => console.error('Erro:', error));
     });
 });
